586c66d9446f0ecf9c610defe83378ce349bae0a
ZJH <ZJH_4236@163.com>
2018-07-27 17:17:48 +0800

    project init

---
+++ src/main/java/com/wust/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.wust.dao;
+
+import com.wust.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
